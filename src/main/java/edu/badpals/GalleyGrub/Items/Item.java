605fcb7dc619acb012c805bbc3dc453c4f7ed0f3
Sergio Prieto García <serprigar@gmail.com>
2024-04-15 10:08:30 +0200

    feat(Item.java): update toString

--- src/main/java/edu/badpals/GalleyGrub/Items/Item.java
+++ src/main/java/edu/badpals/GalleyGrub/Items/Item.java
@@ -13,7 +13,7 @@ public class Item implements Product {
     }
 
     public Item(String name, Double price, String extra) {
-        this.name = name + "w/" + extra;
+        this.name = name + " w/ " + extra;
         this.price = price;
         this.extra = extra;
     }
@@ -37,6 +37,11 @@ public class Item implements Product {
                 .append("....")
                 .append(String.format("%.2f", price()))
                 .append("$");
+        if (extra() != "") {
+                sb.append(" + ")
+                    .append(String.format("%.2f", Prices.prices.get(extra())))
+                        .append("$");
+        }
         return sb.toString();
     }
     public boolean isRegular(){
