6e7529651b1a6ccfee44e01826ebacca8ffee6b3
Sergio Prieto García <serprigar@gmail.com>
2024-04-15 09:31:08 +0200

    feat(Item.java): add Equals HashCode

--- src/main/java/edu/badpals/GalleyGrub/Items/Item.java
+++ src/main/java/edu/badpals/GalleyGrub/Items/Item.java
@@ -1,7 +1,6 @@
 package edu.badpals.GalleyGrub.Items;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Objects;
 
 public class Item implements Product {
     String name = "";
@@ -27,6 +26,10 @@ public class Item implements Product {
         return price;
     }
 
+    public String extra() {
+        return extra;
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
@@ -35,5 +38,21 @@ public class Item implements Product {
                 .append(String.format("%.2f", price()))
                 .append("$");
         return sb.toString();
+    }
+    public boolean isRegular(){
+        return extra.isEmpty() ? true:false;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return Objects.equals(name, item.name) && Objects.equals(price, item.price) && Objects.equals(extra, item.extra);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price, extra);
     }
 }
