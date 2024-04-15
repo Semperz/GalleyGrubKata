9ec24f44291e9981268bc0c887eda8b8ddc9c7a6
Sergio Prieto García <serprigar@gmail.com>
2024-04-15 09:30:36 +0200

    feat(Comanda.java): add Interface

---
+++ src/main/java/edu/badpals/GalleyGrub/Order/Comanda.java
@@ -0,0 +1,21 @@
+package edu.badpals.GalleyGrub.Order;
+
+import edu.badpals.GalleyGrub.Items.Item;
+
+import java.util.List;
+
+public interface Comanda {
+
+    void addItem(String name, Double price);
+    void addItem(String name, Double price, String extra);
+
+    List<Item> itemList();
+
+    int size();
+
+    Double getTotal();
+
+    void updateTotal(Double total);
+
+    String display();
+}
